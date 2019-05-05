5c39e6651354c0417dc794f286b0bbeb45e84724
Ronan Fegan <ronan@Ronans-MBP.hitronhub.home>
2019-05-05 13:08:26 -0700

    Adding logo image and css

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import Navigation from './components/navigation/Navigation'
 import 'tachyons';
+import Logo from './components/logo/Logo'
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/*<ImageLinkForm />
       <FaceRecognition />*/}
     </div>
   );
