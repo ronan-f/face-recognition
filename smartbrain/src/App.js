a43529587c56639b62ab5bb8a86107e4d90160a5
Ronan Fegan <ronan@Ronans-MBP.hitronhub.home>
2019-05-05 13:48:33 -0700

    Adding react particles to create a dynamic background effect

--- smartbrain/src/App.js
+++ smartbrain/src/App.js
@@ -4,14 +4,31 @@ import Navigation from './components/navigation/Navigation'
 import Logo from './components/logo/Logo'
 import Rank from './components/rank/rank'
 import ImageLinkForm from './components/imagelinkform/imageLinkForm'
+import Particles from 'react-particles-js';
 import 'tachyons';
 
-
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 300,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: ['#1E1818']
+    },
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Particles className='particles'
+          params={particlesOptions}
+        />
       <Logo />
       <Rank />
       <ImageLinkForm />
